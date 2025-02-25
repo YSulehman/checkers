f339d33519934b70ff86980a49bc3925ce1d18b8
Yusuf Sulehman <yusuf.sulehman@postgrad.manchester.ac.uk>
2025-02-25 21:45:13 +0000

    alternating moves enforced

--- src/checkers_game.py
+++ src/checkers_game.py
@@ -10,11 +10,18 @@ class Checkers(Board, Pieces):
         self.block_width = self.window_width // 8
         # initialise team one to move first
         self.team_ones_move = True
-        self.last_moved = 'one'
+        # self.last_moved = None
+        # set duration to display messages in milliseconds
+        # self.message_duration = 2000
+        # self.message_time = None
+        # self.show_message = False
 
     def play_game(self):
         # initialise a game 
         pygame.init()
+
+        # initialise font style and font
+        self.font = pygame.font.Font('freesansbold.ttf', 30)
 
         # make a pygame display
         self.display = pygame.display.set_mode((self.window_width, self.window_height))
@@ -30,7 +37,10 @@ class Checkers(Board, Pieces):
 
         # run game 
         while self.run_game:
-             for event in pygame.event.get():
+            # track game_time 
+            # game_time = pygame.time.get_ticks()
+
+            for event in pygame.event.get():
                 # close the game terminal if close button clicked
                 if event.type == pygame.QUIT:
                     self.run_game = False
@@ -41,40 +51,43 @@ class Checkers(Board, Pieces):
                     for point in self.team_one_initial_coordinates:
                         if abs(event.pos[0] - point[1]) <= self.piece_radius and abs(event.pos[1] - point[2]) <= self.piece_radius:
                             self.selected_piece = point
-                            self.team_one_turn = True
-                            # print(f'the selected piece is {self.selected_piece}')
+                        
                             break
                     for point in self.team_two_initial_coordinates:
                         if abs(event.pos[0] - point[1]) <= self.piece_radius and abs(event.pos[1] - point[2]) <= self.piece_radius:
                             self.selected_piece = point
-                            self.team_one_turn = False
-                            # print('here we are')
-                            # print(self.team_one_turn)
+                           
                             break
 
                 elif event.type == pygame.MOUSEBUTTONUP:
                     if self.selected_piece:
-                        # Check if the move is valid
+                        # Check if the move is valid and if it's a normal or king piece moving
                         if self.selected_piece[0] == 'normal':
-                            valid_move = self.valid_normal_move(event.pos, self.selected_piece[1:], self.team_one_turn, self.block_width)
-                            # print(f'valid move for block width {self.block_width}? : {valid_move}')
-                            # print(event.pos)
+                            valid_move = self.valid_normal_move(event.pos, self.selected_piece[1:], self.team_ones_move, self.block_width)
                         else:
-                            valid_move = self.valid_normal_move(event.pos, self.selected_piece[1:], self.team_one_turn, self.block_width, king=True)
+                            valid_move = self.valid_normal_move(event.pos, self.selected_piece[1:], self.team_ones_move, self.block_width, king=True)
 
                         # Update the position if the move is valid
                         if valid_move:
-                            if self.team_one_turn:
-                                self._move_piece(self.display, self.team_one_colour, self.team_one_initial_coordinates, self.selected_piece, event.pos, self.block_width, self.team_one_turn)
+                            if self.team_ones_move:
+                                self._move_piece(self.display, self.team_one_colour, self.team_one_initial_coordinates, self.selected_piece, event.pos, self.block_width, self.team_ones_move)
                                 # update self.team_one_turn
-                                self.team_one_turn = False
-                                # self.last_moved = None
-                                # print('yo')
+                                self.team_ones_move = False
+                                
                             else:
-                                self._move_piece(self.display, self.team_two_colour, self.team_two_initial_coordinates, self.selected_piece, event.pos, self.block_width, self.team_one_turn)
+
+                                self._move_piece(self.display, self.team_two_colour, self.team_two_initial_coordinates, self.selected_piece, event.pos, self.block_width, self.team_ones_move)
                                 # update self.team_one_turn
-                                self.team_one_turn = True
-                                # self.last_moved = 'one'
+                                self.team_ones_move = True
+                                
+                                
+                        else:
+                            pass
+                            # let's display a message to screen for invalid move but for 2 seconds
+                            # if not self.show_message:
+                            #     self.message_time = game_time
+                            #     self.show_message = True
+                    
 
                         # Reset selection after move
                         self.selected_piece = None
@@ -84,22 +97,20 @@ class Checkers(Board, Pieces):
                     # track mouse move movement, mousebuttonup and if move is valid
                 elif event.type == pygame.MOUSEMOTION:
                     pass
-                    # ADD IN PIECE MOVEMENT WITH MOUSE? 
-                    # below code was right idea but incorrect handling of events
-                    # if event.type == pygame.MOUSEBUTTONUP:
-                    #     # check if the attempted move is valid and update accordingly.
-                    #     if point[0] == 'normal':
-                    #         valid_move = self.valid_normal_move(event.pos, point[1:], team_one, self.block_width)
-                    #     else:
-                    #         valid_move = self.valid_normal_move(event.pos, point[1:], team_one, self.block_width, king=True)
-                    #     # need to update position, draw and update display. 
-                    #     if valid_move and team_one:
-                    #         # update piece location
-                    #         self._move_piece(self.display, self.team_one_colour, self.team_one_initial_coordinates, point, event.pos, 
-                    #                             self.block_width)
-                    #     elif valid_move and not team_one:
-                    #         self._move_piece(self.display, self.team_two_colour, self.team_two_initial_coordinates, point, event.pos, 
-                    #                             self.block_width)
 
-                    #     # update display last
-                    #     pygame.display.update()
+                
+            # if self.show_message and  game_time - self.message_time < self.message_duration:
+            #     # print(game_time - self.message_time)
+            #     # if game_time - self.message_time < self.message_duration:
+            #     self._display_message('Invalid move!')
+            #     pygame.display.update()
+            # else:
+            #     self.show_message = False
+                  
+    def _display_message(self, text: str):
+        # render text and box containing text
+        text_obj = self.font.render(text, True, self.black, self.white)
+        textRect = text_obj.get_rect()
+        textRect.center = (self.window_width // 2, self.window_height // 2)
+
+        self.display.blit(text_obj, textRect)
